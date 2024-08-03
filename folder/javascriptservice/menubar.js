c74562871f0dbdb87aac2c0bcfba01f01ca032c2
programming-idle <thristanezraa32.gaming@gmail.com>
2024-08-03 21:13:27 +0700

    Add files via upload

---
+++ folder/javascriptservice/menubar.js
@@ -0,0 +1,15 @@
+function websiteSettings(){
+  alert("This is the Beta version of ProgrammingIdle and it may not work properly. We're sorry!");
+}
+
+function websiteInfo(){
+  alert("This is the Beta version of ProgrammingIdle and it may not work properly. We're sorry!");
+}
+
+function gameSaved(){
+  alert("This is the Beta version of ProgrammingIdle and it may not work properly. We're sorry!");
+}
+
+function gameLoaded(){
+  alert("This is the Beta version of ProgrammingIdle and it may not work properly. We're sorry!");
+}
